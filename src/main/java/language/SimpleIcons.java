99e4356150693fb083ef5f17f6207479f484d5e3
zigzzzag <maximnikiforov1991@gmail.com>
2017-09-03 20:04:48 +0700

    add new language and types

---
+++ src/main/java/language/SimpleIcons.java
@@ -0,0 +1,10 @@
+package language;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class SimpleIcons {
+
+    public static final Icon FILE = IconLoader.getIcon("/icons/sos.png");
+}
---
+++ src/main/java/language/SimpleLanguage.java
@@ -0,0 +1,12 @@
+package language;
+
+import com.intellij.lang.Language;
+
+public class SimpleLanguage extends Language {
+
+    public static final SimpleLanguage INSTANCE = new SimpleLanguage();
+
+    private SimpleLanguage() {
+        super("Simple");
+    }
+}
